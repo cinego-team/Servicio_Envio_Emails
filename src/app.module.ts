17e2d01474908d7f00f5c27e8939ffc5b3d6af17
Ramiro Lynch <ramirolynch@hotmail.com>
2026-02-18 19:57:20 -0300

    fix env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MailModule } from './mail/mail.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [MailModule],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }), MailModule],
     controllers: [AppController],
     providers: [AppService],
 })
